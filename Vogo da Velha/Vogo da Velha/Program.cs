0912a8b55308a2013d9b6087bcb2298275b17406
Wellington Peres Vasconcelos <wellingtonpv.100@gmail.com>
2021-04-26 06:50:09 -0300

    Jogo da velha

---
+++ Vogo da Velha/Vogo da Velha/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vogo_da_Velha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matriz = new string[3, 3];
+            string turno = "X";
+
+            int index = 1;
+
+            int tentativas = 0;
+
+            // Alimentando a Matriz.
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = index.ToString();
+                    index++;
+                }
+            }
+
+            // Imprimir a matriz.
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($" [{matriz[i, j]}] ");
+                }
+                Console.WriteLine();
+            }
+
+            string jogada = Console.ReadLine();
+
+            Console.Clear();
+
+            // Começa o jogo.
+            while(tentativas < 9)
+            {
+                // Substituir o valor na sua respectiva casa.
+                for (int i = 0; i < matriz.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriz.GetLength(1); j++)
+                    {
+                        if (matriz[i, j] == jogada)
+                        {
+                            matriz[i, j] = turno;
+                        }
+                    }
+                }
+
+                // Imprimir a matriz.
+                for (int i = 0; i < matriz.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriz.GetLength(1); j++)
+                    {
+                        Console.Write($" [{matriz[i, j]}] ");
+                    }
+                    Console.WriteLine();
+                }
+
+                jogada = Console.ReadLine();
+
+                if (turno == "X")
+                {
+                    turno = "O";
+                }
+                else
+                {
+                    turno = "X";
+                }
+
+                tentativas++;
+
+                Console.Clear();          
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
